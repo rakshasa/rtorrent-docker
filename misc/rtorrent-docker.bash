94947f06c57348de77824b784d3c96f4b82b6dd7
Jari Sundell <sundell.software@gmail.com>
2020-01-03 00:25:06 +0900

    Improved build process. (#3)

--- misc/rtorrent-docker.bash
+++ misc/rtorrent-docker.bash
@@ -60,7 +60,12 @@ _rtorrent_docker__rdo() {
 
 _rtorrent_docker__rdo_build() {
   if [[ "${word}" == -* ]]; then
-    flags=(--compiler#-c --dry-run --help)
+    flags=(
+      --compiler#-c
+      --dry-run
+      --rebuild
+      --help
+    )
     arg_funcs=(
       --compiler##rdo_compilers
     )
@@ -69,10 +74,13 @@ _rtorrent_docker__rdo_build() {
       all
       check
       compile
+      libtorrent-dev
       libtorrent-check
       libtorrent-compile
+      libtorrent-complete
       rtorrent-check
       rtorrent-compile
+      rtorrent-dev
     )
   fi
 }
