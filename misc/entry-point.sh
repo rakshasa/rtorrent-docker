7dec7caf06638b96c407abffa71b48457f5416f6
rakshasa <sundell.software@gmail.com>
2020-06-21 17:56:15 +0900

    Added rtorrent run template.

--- misc/entry-point.sh
+++ misc/entry-point.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
-echo "staging" > /stage/self/state
+echo "staging" > "/stage/self/state"
 
+SECONDS=0
 
+while true; do
+  if (( ${SECONDS} > 600 )); then
+    echo "error" > "/stage/self/state"
+    echo "staging_timeout" > "/stage/self/error"
+    exit 0
+  fi
 
-echo "running" > /stage/self/state
+  if [[ ! -f "/stage/self/signal" ]] || [[ "$(cat "/stage/self/signal")" == "stage" ]]; then
+    sleep 0.1
+    continue
+  fi
 
-"${@}"
+  if [[ "$(cat "/stage/self/signal")" != "deploy" ]]; then
+    echo "error" > "/stage/self/state"
+    echo "staging_unexpected_state" > "/stage/self/error"
+    exit 0
+  fi
 
-echo "exited" > /stage/self/state
+  break
+done
+
+echo "running" > "/stage/self/state"
+
+if ! "/stage/self/run" &> "/stage/self/log"; then
+  echo "error" > "/stage/self/state"
+  echo "run_error" > "/stage/self/error"
+  exit 0
+fi
+
+echo "exited" > "/stage/self/state"
