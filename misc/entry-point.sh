c113be9651d9ede702a48618d93a588c2cb6ca4a
rakshasa <sundell.software@gmail.com>
2020-06-21 18:18:39 +0900

    Minor dependency fixes.

--- misc/entry-point.sh
+++ misc/entry-point.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-echo "staging" > "/stage/self/state"
+echo "staging" > "/run/self/state"
 
 SECONDS=0
 
 while true; do
   if (( ${SECONDS} > 600 )); then
-    echo "error" > "/stage/self/state"
-    echo "staging_timeout" > "/stage/self/error"
+    echo "error" > "/run/self/state"
+    echo "staging_timeout" > "/run/self/error"
     exit 0
   fi
 
-  if [[ ! -f "/stage/self/signal" ]] || [[ "$(cat "/stage/self/signal")" == "stage" ]]; then
+  if [[ ! -f "/run/self/signal" ]] || [[ "$(cat "/run/self/signal")" == "stage" ]]; then
     sleep 0.1
     continue
   fi
 
-  if [[ "$(cat "/stage/self/signal")" != "deploy" ]]; then
-    echo "error" > "/stage/self/state"
-    echo "staging_unexpected_state" > "/stage/self/error"
+  if [[ "$(cat "/run/self/signal")" != "deploy" ]]; then
+    echo "error" > "/run/self/state"
+    echo "staging_unexpected_state" > "/run/self/error"
     exit 0
   fi
 
   break
 done
 
-echo "running" > "/stage/self/state"
+echo "running" > "/run/self/state"
 
-if ! "/stage/self/run" &> "/stage/self/log"; then
-  echo "error" > "/stage/self/state"
-  echo "run_error" > "/stage/self/error"
+if ! "/run/self/run" &> "/run/self/log"; then
+  echo "error" > "/run/self/state"
+  echo "run_error" > "/run/self/error"
   exit 0
 fi
 
-echo "exited" > "/stage/self/state"
+echo "exited" > "/run/self/state"
